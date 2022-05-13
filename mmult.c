41bbbaaefc7b87f014eae035205880e2618ba8e3
ZuhChar <zcrimmel@uwyo.edu>
2022-05-13 15:36:29 -0600

    So it beginns

---
+++ mmult.c
@@ -0,0 +1,14 @@
+//
+// wyshell.c
+// Author: Zachary Crimmel
+// Date: May 13, 2022
+//
+// COSC 3750, Homework 11
+//
+// This is a simple program to calculate the matrix multiplication of two matrices.
+//
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
