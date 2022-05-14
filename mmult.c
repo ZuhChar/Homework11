3f3732042c49428d3dfd871391e41c3edb746f84
ZuhChar <zcrimmel@uwyo.edu>
2022-05-13 21:19:39 -0600

    l

--- mmult.c
+++ mmult.c
@@ -93,13 +93,13 @@ int main(int argc, char **argv)
         //         }
         //     }
         // }
-        for (int i = 0; i < 3; i++)
-        {
-            for (int j = 0; j < 3; j++)
-            {
-                printf("%d ", a[i][j]);
-            }
-            printf("\n");
-        }
+        // for (int i = 0; i < 3; i++)
+        // {
+        //     for (int j = 0; j < 3; j++)
+        //     {
+        //         printf("%d ", a[i][j]);
+        //     }
+        //     printf("\n");
+        // }
     }
 }
